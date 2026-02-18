19b8f740bd737c9be5fa1e8da56836afcb68f0a3
Molt <molt@example.com>
2026-02-18 19:02:18 +0900

    fix: use absolute path for CACHE_DIR

--- local_updater.py
+++ local_updater.py
@@ -13,7 +13,7 @@ URLS = {
     "MUFG": "https://www.cr.mufg.jp/mufgcard/point/global/save/convenience_store/index.html"
 }
 
-CACHE_DIR = "otoku-checker/html_cache"
+CACHE_DIR = os.path.join(os.path.dirname(__file__), "html_cache")
 
 def clean_html_aggressive(html_text):
     import re
